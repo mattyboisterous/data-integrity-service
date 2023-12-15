6a95e2eb65f41028b43448bd68cc9728ff683365
Matt Warwick <mattyboisterous@gmail.com>
2023-12-15 15:27:45 +1000

    Ok - one last thing for the day!

---
+++ DataIntegrityService.Core/Services/ChangeTracking/LocalChangeTrackingService.cs
@@ -0,0 +1,45 @@
+﻿using DataIntegrityService.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataIntegrityService.Core.Services.ChangeTracking
+{
+  public class LocalChangeTrackingService : IChangeTrackingService
+  {
+    public string Key => "LocalChangeTrackingService";
+    public bool IsInitialised { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public void Initialise()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task FlagAsCompleted(AllModelDataSetChange item)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task FlagAsPoison(AllModelDataSetChange item)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task FlushAllPendingChanges()
+    {
+      throw new NotImplementedException();
+    }
+
+    public AllModelDataSetChange GetNextChange()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task IncrementAttempt(AllModelDataSetChange item)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
